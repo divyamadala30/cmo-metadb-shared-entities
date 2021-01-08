f61ec0018e619d6176156f9d523c07ea35cf538e
Angelica Ochoa <ochoaa@mskcc.org>
2021-01-08 16:20:24 -0500

    Update SampleManifest, add LimsRest subclasses for SampleManifest
    Signed-off-by: Angelica Ochoa <ochoaa@mskcc.org>

--- src/main/java/org/mskcc/cmo/shared/SampleManifest.java
+++ src/main/java/org/mskcc/cmo/shared/SampleManifest.java
@@ -1,98 +1,100 @@
 package org.mskcc.cmo.shared;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  *
  * @author ochoaa
  */
-public class SampleManifest extends SampleMetadata {
-    protected String cmoInfoIgoId;
-    protected String cmoSampleName;
-    protected String sampleName;
-    protected String cmoSampleClass;
-    protected String oncotreeCode;
-    protected String collectionYear;
-    protected String tubeId;
-    protected String cfDNA2dBarcode;
+@JsonIgnoreProperties(value = { "cmoInfoIgoId" })
+public class SampleManifest {
+    private String igoId;
+    // left out of pipeline JSON
+    private String cmoInfoIgoId;
+    // aka "Corrected CMO Sample ID", but not an ID by normal database standards
+    private String cmoSampleName;
+    private String sampleName;
+    private String cmoSampleClass;
+    private String cmoPatientId;
+    private String investigatorSampleId;
+    private String oncotreeCode;
+    private String tumorOrNormal;
+    private String tissueLocation;
+    private String specimenType;
+    private String sampleOrigin;
+    private String preservation;
+    private String collectionYear;
+    private String sex;
+    private String species;
+    private String tubeId;
+    private String cfDNA2dBarcode;
+    private String baitSet;
+    private List<QcReport> qcReports;
+    private List<Library> libraries;
 
-    public SampleManifest() {
-        super();
-    }
+    public SampleManifest() {}
 
     /**
-     * SampleManifest constructor.
-     * @param mrn
-     * @param cmoPatientId
-     * @param cmoSampleId
+     * SampleManifestConstructor.
      * @param igoId
-     * @param investigatorSampleId
-     * @param species
-     * @param sex
-     * @param tumorOrNormal
-     * @param sampleType
-     * @param preservation
-     * @param tumorType
-     * @param parentTumorType
-     * @param specimenType
-     * @param sampleOrigin
-     * @param tissueSource
-     * @param tissueLocation
-     * @param recipe
-     * @param baitset
-     * @param fastqPath
-     * @param principalInvestigator
-     * @param ancestorSample
-     * @param doNotUse
-     * @param sampleStatus
      * @param cmoInfoIgoId
      * @param cmoSampleName
      * @param sampleName
      * @param cmoSampleClass
+     * @param cmoPatientId
+     * @param investigatorSampleId
      * @param oncotreeCode
+     * @param tumorOrNormal
+     * @param tissueLocation
+     * @param specimenType
+     * @param sampleOrigin
+     * @param preservation
      * @param collectionYear
+     * @param sex
+     * @param species
      * @param tubeId
      * @param cfDNA2dBarcode
+     * @param baitSet
+     * @param qcReports
+     * @param libraries
      */
-    public SampleManifest(String mrn, String cmoPatientId, String cmoSampleId, String igoId,
-            String investigatorSampleId, String species, String sex, String tumorOrNormal, String sampleType,
-            String preservation, String tumorType, String parentTumorType, String specimenType,
-            String sampleOrigin, String tissueSource, String tissueLocation, String recipe, String baitset,
-            String fastqPath, String principalInvestigator, String ancestorSample, boolean doNotUse,
-            String sampleStatus, String cmoInfoIgoId, String cmoSampleName, String sampleName,
-            String cmoSampleClass, String oncotreeCode, String collectionYear, String tubeId,
-            String cfDNA2dBarcode) {
-        super(mrn,
-            cmoPatientId,
-            cmoSampleId,
-            igoId,
-            investigatorSampleId,
-            species,
-            sex,
-            tumorOrNormal,
-            sampleType,
-            preservation,
-            tumorType,
-            parentTumorType,
-            specimenType,
-            sampleOrigin,
-            tissueSource,
-            tissueLocation,
-            recipe,
-            baitset,
-            fastqPath,
-            principalInvestigator,
-            ancestorSample,
-            doNotUse,
-            sampleStatus);
+    public SampleManifest(String igoId, String cmoInfoIgoId, String cmoSampleName, String sampleName,
+            String cmoSampleClass, String cmoPatientId, String investigatorSampleId, String oncotreeCode,
+            String tumorOrNormal, String tissueLocation, String specimenType, String sampleOrigin,
+            String preservation, String collectionYear, String sex, String species, String tubeId,
+            String cfDNA2dBarcode, String baitSet, List<QcReport> qcReports, List<Library> libraries) {
+        this.igoId = igoId;
         this.cmoInfoIgoId = cmoInfoIgoId;
         this.cmoSampleName = cmoSampleName;
         this.sampleName = sampleName;
         this.cmoSampleClass = cmoSampleClass;
+        this.cmoPatientId = cmoPatientId;
+        this.investigatorSampleId = investigatorSampleId;
         this.oncotreeCode = oncotreeCode;
+        this.tumorOrNormal = tumorOrNormal;
+        this.tissueLocation = tissueLocation;
+        this.specimenType = specimenType;
+        this.sampleOrigin = sampleOrigin;
+        this.preservation = preservation;
         this.collectionYear = collectionYear;
+        this.sex = sex;
+        this.species = species;
         this.tubeId = tubeId;
         this.cfDNA2dBarcode = cfDNA2dBarcode;
+        this.baitSet = baitSet;
+        this.qcReports = qcReports;
+        this.libraries = libraries;
+    }
+
+    public String getIgoId() {
+        return igoId;
+    }
+
+    public void setIgoId(String igoId) {
+        this.igoId = igoId;
     }
 
     public String getCmoInfoIgoId() {
@@ -127,6 +129,22 @@ public class SampleManifest extends SampleMetadata {
         this.cmoSampleClass = cmoSampleClass;
     }
 
+    public String getCmoPatientId() {
+        return cmoPatientId;
+    }
+
+    public void setCmoPatientId(String cmoPatientId) {
+        this.cmoPatientId = cmoPatientId;
+    }
+
+    public String getInvestigatorSampleId() {
+        return investigatorSampleId;
+    }
+
+    public void setInvestigatorSampleId(String investigatorSampleId) {
+        this.investigatorSampleId = investigatorSampleId;
+    }
+
     public String getOncotreeCode() {
         return oncotreeCode;
     }
@@ -135,12 +153,68 @@ public class SampleManifest extends SampleMetadata {
         this.oncotreeCode = oncotreeCode;
     }
 
+    public String getTumorOrNormal() {
+        return tumorOrNormal;
+    }
+
+    public void setTumorOrNormal(String tumorOrNormal) {
+        this.tumorOrNormal = tumorOrNormal;
+    }
+
+    public String getTissueLocation() {
+        return tissueLocation;
+    }
+
+    public void setTissueLocation(String tissueLocation) {
+        this.tissueLocation = tissueLocation;
+    }
+
+    public String getSpecimenType() {
+        return specimenType;
+    }
+
+    public void setSpecimenType(String specimenType) {
+        this.specimenType = specimenType;
+    }
+
+    public String getSampleOrigin() {
+        return sampleOrigin;
+    }
+
+    public void setSampleOrigin(String sampleOrigin) {
+        this.sampleOrigin = sampleOrigin;
+    }
+
+    public String getPreservation() {
+        return preservation;
+    }
+
+    public void setPreservation(String preservation) {
+        this.preservation = preservation;
+    }
+
     public String getCollectionYear() {
         return collectionYear;
     }
 
     public void setCollectionYear(String collectionYear) {
         this.collectionYear = collectionYear;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
     }
 
     public String getTubeId() {
@@ -157,6 +231,44 @@ public class SampleManifest extends SampleMetadata {
 
     public void setCfDNA2dBarcode(String cfDNA2dBarcode) {
         this.cfDNA2dBarcode = cfDNA2dBarcode;
+    }
+
+    public String getBaitSet() {
+        return baitSet;
+    }
+
+    public void setBaitSet(String baitSet) {
+        this.baitSet = baitSet;
+    }
+
+    /**
+     * Returns empty array list if field is null.
+     * @return
+     */
+    public List<QcReport> getQcReports() {
+        if (qcReports == null) {
+            this.qcReports = new ArrayList<>();
+        }
+        return qcReports;
+    }
+
+    public void setQcReports(List<QcReport> qcReports) {
+        this.qcReports = qcReports;
+    }
+
+    /**
+     * Returns empty array list if field is null.
+     * @return
+     */
+    public List<Library> getLibraries() {
+        if (libraries == null) {
+            this.libraries = new ArrayList<>();
+        }
+        return libraries;
+    }
+
+    public void setLibraries(List<Library> libraries) {
+        this.libraries = libraries;
     }
 
     @Override
